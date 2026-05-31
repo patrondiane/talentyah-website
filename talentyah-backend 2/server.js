6d482d01ebccc8e8cbba67abffc5b846a2bdecee
patrondiane <patrondiane@hotmail.com>
2026-06-01 01:10:36 +0200

    deloiement

--- talentyah-backend 2/server.js
+++ talentyah-backend 2/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors    = require('cors');
 const path    = require('path');
 const db      = require('./db');
+require('dotenv').config();
 
 const app  = express();
 const PORT = process.env.PORT || 4000;
